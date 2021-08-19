e5b0c17688c17da72292cb4dd49193e79906b7d0
ma1gus <now-kirill@ya.ru>
2021-08-19 14:01:43 +0300

    Android casua game
    Android casua game

---
+++ Fire Ball 3D/Assets/Scripts/Tank.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Tank : MonoBehaviour
+{
+    [SerializeField] private Transform _shootPoint;
+    [SerializeField] private Bullet _bulletTemplate;
+    [SerializeField] private float _delayBetweenShoot;
+    [SerializeField] private float _recoilDistance;
+
+    private float _timeAfterShoot;
+
+    private void Update()
+    {
+        _timeAfterShoot += Time.deltaTime;
+
+        if(Input.GetMouseButton(0))
+        {
+            if(_timeAfterShoot > _delayBetweenShoot)
+            {
+                Shoot();
+                transform.DOMoveZ(transform.position.z - _recoilDistance, _delayBetweenShoot / 2).SetLoops(2, LoopType.Yoyo);
+                _timeAfterShoot = 0;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(_bulletTemplate, _shootPoint.position, Quaternion.identity);
+    }
+}
---
+++ Fire Ball 3D/Assets/Scripts/TowerSizeView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class TowerSizeView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _sizeView;
+    [SerializeField] private Tower _tower;
+
+    private void OnEnable()
+    {
+        _tower.SizeUpdate += OnSizeUpdated;
+    }
+
+    private void OnDisable()
+    {
+        _tower.SizeUpdate -= OnSizeUpdated;
+    }
+
+    private void OnSizeUpdated(int size)
+    {
+        _sizeView.text = size.ToString();
+    }
+    
+}
